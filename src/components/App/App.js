5d1d50cae94f266a251749a00174fc5d99a0e8c6
Adam Boerhave <arboerhave@gmail.com>
2020-10-22 20:12:00 -0500

    getting array of 3 objects in terminal - need to fix error

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,36 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import Header from '../Header/Header'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    pictureArray: []
+  }
+
+  componentDidMount = () => {
+    console.log('app.js mounted');
+    this.getPictures();
+  }
+
+  // need GET request
+  getPictures = () => {
+    console.log('in getPictures');
+    
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) => {
+      console.log('response', response.data);
+      this.setState({
+        pictureArray: response.data
+      }).catch((error) => {
+        console.log('error', error);
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +38,9 @@ class App extends Component {
         <br/>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
+        {this.state.pictureArray.map((picture) => {
+          return <li> {picture.description}</li>
+        })}
       </div>
     );
   }
