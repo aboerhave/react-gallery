8065dda37c70f286cd49b8da4ef9559a5301225a
Adam Boerhave <arboerhave@gmail.com>
2020-10-22 19:37:30 -0500

    Header componentized

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../Header/Header'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of my life</h1>
-        </header>
+        <Header />
         <br/>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
