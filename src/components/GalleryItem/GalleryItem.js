ffdd51d013bc71a060a784d18ee9d4c4363567d0
Adam Boerhave <arboerhave@gmail.com>
2020-10-23 19:06:46 -0500

    the images click and display in console- YAY

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -3,10 +3,24 @@ import './GalleryItem.css';
 
 
 class GalleryItem extends Component {
+
+    state = {
+        pictureOn: true
+    }
+
+    imageToggle = (event) => {
+        event.preventDefault();
+        console.log('clicked');
+        
+    }
+
     render(){
         return (
-            <div className="PictureDiv">
-                <img src={this.props.picture.path} alt={this.props.picture.description} width="200px" />
+            <div className="PictureDiv" >
+                <img src={this.props.picture.path} 
+                alt={this.props.picture.description} 
+                width="200px"  onClick={this.imageToggle}
+                />
             </div>
         )
     }
