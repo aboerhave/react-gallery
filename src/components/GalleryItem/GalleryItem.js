fb18bca09911dd16cb63c08c4cdad355bc1b82e8
Adam Boerhave <arboerhave@gmail.com>
2020-10-23 19:58:52 -0500

    pictures click

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -8,20 +8,46 @@ class GalleryItem extends Component {
         pictureOn: true
     }
 
+    renderPictures = (picture) => {
+        if(this.state.pictureOn){
+        return (
+            <div className="PictureDivOn" >
+                <img src={picture.path} 
+                alt={picture.description} 
+                width="200px"  onClick={this.imageToggle}
+                />
+            </div>
+            )
+        }
+        else {
+            return (
+                <div className="PictureDivOff" >
+                    <img src={picture.path} 
+                    alt={picture.description} 
+                    width="200px"  onClick={this.imageToggle}
+                    />
+                </div>
+            )
+        }
+    }
+
     imageToggle = (event) => {
         event.preventDefault();
-        console.log('clicked');
-        
+        console.log('clicked', this.state.pictureOn);
+        this.setState({
+            pictureOn: !this.state.pictureOn
+        })
     }
 
     render(){
         return (
-            <div className="PictureDiv" >
-                <img src={this.props.picture.path} 
-                alt={this.props.picture.description} 
-                width="200px"  onClick={this.imageToggle}
-                />
-            </div>
+            this.renderPictures(this.props.picture)
+            // <div className="PictureDiv" >
+            //     <img src={this.props.picture.path} 
+            //     alt={this.props.picture.description} 
+            //     width="200px"  onClick={this.imageToggle}
+            //     />
+            // </div>
         )
     }
 }
