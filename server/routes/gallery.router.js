7be9737e43b24dc30aa9ee3ace15d4a17fafbb47
Adam Boerhave <arboerhave@gmail.com>
2020-10-25 14:50:50 -0500

    comments in gallery router

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,18 @@
+// This is the gallery router file for the Week 10 assignment for Prime Digital Academy, created by Adam Boerhave, 
+// 10/22/1010 - 10/25/2020
+
+// requires
 const express = require('express');
 const router = express.Router();
+// Not used anymore now that data is store in db
 // const galleryItems = require('../modules/gallery.data');
 const pool = require('../modules/pool.js');
 
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
-// PUT Route
+// PUT Route to increase likes by 1 for pictureId
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
     const galleryId = req.params.id;
     
     let queryText = `UPDATE "cool_pictures"
@@ -20,11 +24,10 @@ router.put('/like/:id', (req, res) => {
     }).catch((error) => {
         console.log('error', error);
         res.sendStatus(500);
-    })
-    
+    });
 }); // END PUT Route
 
-// GET Route
+// GET Route to get all previously stored picture data
 router.get('/', (req, res) => {
     let queryText = `SELECT * FROM cool_pictures ORDER BY "id"`;
     pool.query(queryText).then((result) => {
@@ -36,6 +39,7 @@ router.get('/', (req, res) => {
     });
 }); // END GET Route
 
+// POST route to post new picture data to database
 router.post('/', (req, res) => {
     console.log('req.body', req.body);
     let queryText = `INSERT INTO "cool_pictures" 
@@ -48,10 +52,10 @@ router.post('/', (req, res) => {
     }).catch((error) => {
         console.log('error', error);
         res.sendStatus(500);
-    })
-})
+    });
+});
 
-// delete route for deleting an item
+// DELETE route for deleting an item
 router.delete('/:id', (req,res) => {
     let queryText = `DELETE FROM "cool_pictures" WHERE "id" = $1;`;
 
@@ -61,7 +65,7 @@ router.delete('/:id', (req,res) => {
     }).catch((error) => {
         console.log('error', error);
         res.sendStatus(500);
-    })
-})
+    });
+});
 
 module.exports = router;
