4817bd3b5a8f607a93bfdbaa2bfbae44b1c0d107
Adam Boerhave <arboerhave@gmail.com>
2020-10-24 21:50:18 -0500

    object created when form submitted with properties path and description

--- src/components/GalleryForm/GalleryForm.js
+++ src/components/GalleryForm/GalleryForm.js
@@ -6,14 +6,22 @@ class GalleryForm extends Component {
 state = {
     newPicture: {
         path: '',
-        desctiption: ''
+        description: ''
     }
+}
+
+handleChange = (event, property) => {
+    this.setState({
+        newPicture: {
+            ...this.state.newPicture,
+            [property]: event.target.value
+        }
+    })
 }
 
 addPicture = (event) => {
     event.preventDefault();
-    console.log('clicked');
-    
+    console.log('clicked', this.state.newPicture);
 }
 
 render() {
@@ -23,9 +31,13 @@ render() {
         <form onSubmit={this.addPicture}>
             <label htmlFor="pathBox">Path to Image</label>
             <input type="text" placeholder="Path to Image" id="pathBox"
-            /*onChange={(event) => this.state.handleChange(event, 'path')} */
-             />
-            <button>Submit</button>
+                onChange={(event) => this.handleChange(event, 'path')}
+                value={this.state.newPicture.path}/>
+            <label htmlFor="descriptionBox">Decription of Image</label>
+            <input type="text" placeholder="Description of Image" id="descriptionBox"
+                onChange={(event) => this.handleChange(event, 'description')}
+                value={this.state.newPicture.description} maxLength="200"/>
+            <button>Submit New Picture</button>
         </form>
         </>
         )
