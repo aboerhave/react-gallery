30abaa0aed002263acee26bfb361545100834a12
Adam Boerhave <arboerhave@gmail.com>
2020-10-25 15:14:37 -0500

    comments in GalleryFormjs and Appjs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,6 @@
+// This is the App.js file for the Week 10 assignment for Prime Digital Academy, created by Adam Boerhave, 
+// 10/22/1010 - 10/25/2020
+
 import axios from 'axios';
 import React, { Component } from 'react';
 import Header from '../Header/Header';
@@ -11,12 +14,13 @@ class App extends Component {
     pictureArray: []
   }
 
+  // function for page load
   componentDidMount = () => {
     console.log('app.js mounted');
-    this.getPictures();
+    this.getPictures(); // calls get request right away
   }
 
-  // GET request
+  // GET request function to get all stored pictures
   getPictures = () => {
     console.log('in getPictures');
     
@@ -30,21 +34,22 @@ class App extends Component {
       })
     }).catch((error) => {
       console.log('error', error);
+      alert('There was an error.  Try again later.');
     });
   }
 
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header />  
         <br/>
+        {/* GalleryForm component with getPictures get request function sent down as prop */}
         <GalleryForm getPictures={this.getPictures}/>
+        {/* GalleryList component with getPictures function and pictureArray sent down as props */}
         <GalleryList pictureArray={this.state.pictureArray} getPictures={this.getPictures}/>
       </div>
     );
   }
 }
-
-
 
 export default App;
--- src/components/GalleryForm/GalleryForm.js
+++ src/components/GalleryForm/GalleryForm.js
@@ -1,3 +1,6 @@
+// This is the GalleryForm file for the Week 10 assignment for Prime Digital Academy, created by Adam Boerhave, 
+// 10/22/1010 - 10/25/2020
+
 import axios from 'axios';
 import React, { Component } from 'react';
 import './GalleryForm.css';
@@ -5,6 +8,7 @@ import './GalleryForm.css';
 
 class GalleryForm extends Component {
 
+// empty state created
 state = {
     newPicture: {
         path: '',
@@ -12,22 +16,25 @@ state = {
     }
 }
 
+// handleChange function for form input
 handleChange = (event, property) => {
     this.setState({
         newPicture: {
             ...this.state.newPicture,
             [property]: event.target.value
         }
-    })
+    });
 }
 
+// addPicture function handles axios request to post picture data to database
 addPicture = (event) => {
-    event.preventDefault();
+    event.preventDefault(); // no page refresh
+    // checks to make sure something in the box
     if (!this.state.newPicture.path || !this.state.newPicture.description) {
         alert('Please make sure you fill in all of the information');
         return;
     }
-    console.log('clicked', this.state.newPicture);
+
     axios({
         method: 'POST',
         url: '/gallery',
@@ -46,18 +53,21 @@ addPicture = (event) => {
     }).catch((error) => {
         console.log('error', error);
     });
-}
+}   // end addPicture fn
 
 render() {
     return (
         <>
         <h3>Add a new image to the collection:</h3>
+        {/* form handles all the inputs on the button click */}
         <form onSubmit={this.addPicture}>
+            {/* PATH */}
             <label htmlFor="pathBox">Path to Image:</label>
             <input type="text" placeholder="Path to Image" id="pathBox"
                 onChange={(event) => this.handleChange(event, 'path')}
                 value={this.state.newPicture.path} ref="path" autoFocus/>
 
+            {/* DESCRIPTION */}
             <label htmlFor="descriptionBox">Description of Image:</label>
             <input type="text" placeholder="Description of Image" id="descriptionBox"
                 onChange={(event) => this.handleChange(event, 'description')}
