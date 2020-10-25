d13c9e22557000624519cb171cf4ff9fe16d53ac
Adam Boerhave <arboerhave@gmail.com>
2020-10-24 19:42:53 -0500

    changed picture names from numbers

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,7 +1,7 @@
 const galleryItems = [
     { id: 1, path: 'images/Lake.jpg', description: "Lake Alexander from my family's boat", likes: 0 },
-    { id: 2, path: 'images/Wild_Thing.jpg', description: "Wild Thing rollercoaster at Valleyfair, MN", likes: 0 },
-    { id: 3, path: 'images/Steel_Vengeance.jpg', description: "Steel Vengeance and Maverick rollercoasters at Cedar Point, OH", likes: 0 }
+    { id: 2, path: 'images/Wild_Thing.jpg', description: "Wild Thing roller coaster at Valleyfair, MN", likes: 0 },
+    { id: 3, path: 'images/Steel_Vengeance.jpg', description: "Steel Vengeance and Maverick roller coasters at Cedar Point, OH", likes: 0 }
 
 ];
 
